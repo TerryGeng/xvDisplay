2623c750660153d8fc3331b9d9b1d1857c288d6a
TerryGeng <gengyanda@gmail.com>
2013-08-08 19:37:02 +0800

    [AD] Added a debug window.

---
+++ xvDisplay/DebugForm.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DebugForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TextBox1.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.LawnGreen
+        Me.TextBox1.Location = New System.Drawing.Point(-2, -2)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(502, 293)
+        Me.TextBox1.TabIndex = 0
+        '
+        'DebugForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(497, 287)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "DebugForm"
+        Me.Text = "Debug - Standard I/O"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+End Class
---
+++ xvDisplay/DebugForm.vb
@@ -0,0 +1,19 @@
+﻿Public Class DebugForm
+
+    Private Sub DebugForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Top = -2
+        TextBox1.Left = -2
+        TextBox1.Height = Me.ClientSize.Height + 4
+        TextBox1.Width = Me.ClientSize.Width + 4
+    End Sub
+
+    Public Sub Print(ByVal str As String)
+        TextBox1.AppendText(String.Format("[{0}] ", Date.Now.ToString("HH:mm:ss.fff")))
+        TextBox1.AppendText(str)
+    End Sub
+
+    Private Sub DebugForm_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Resize
+        TextBox1.Height = Me.ClientSize.Height + 4
+        TextBox1.Width = Me.ClientSize.Width + 4
+    End Sub
+End Class

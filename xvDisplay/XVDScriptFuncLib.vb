4f3a94f8213830066163cabb946aad948d9789cc
TerryGeng <gengyanda@gmail.com>
2013-08-10 10:35:15 +0800

    [AD] Supported more script functions.

--- xvDisplay/XVDScriptFuncLib.vb
+++ xvDisplay/XVDScriptFuncLib.vb
@@ -10,6 +10,9 @@ Public Class ScriptFuncLib
         Controller.ScriptEngine.AddFunction(New LibFunction("xvdreadglobalconf", AddressOf XVDReadGlobalConf, 1))
         Controller.ScriptEngine.AddFunction(New LibFunction("xvdremovetempconf", AddressOf XVDRemoveTempConf, 1))
         Controller.ScriptEngine.AddFunction(New LibFunction("xvddrawitemset", AddressOf XVDDrawItemSet, 1))
+        Controller.ScriptEngine.AddFunction(New LibFunction("xvddraw", AddressOf XVDDraw))
+        Controller.ScriptEngine.AddFunction(New LibFunction("xvdchangeitemres", AddressOf XVDChangeItemRes, 4))
+        Controller.ScriptEngine.AddFunction(New LibFunction("xvdsetdrawingupdateflag", AddressOf XVDSetDrawingUpdateFlag))
         Controller.ScriptEngine.AddFunction(New LibFunction("xvdexit", AddressOf XVDExit))
 
     End Sub
@@ -40,8 +43,72 @@ Public Class ScriptFuncLib
         Return Nothing
     End Function
 
+    Public Function XVDDraw(ByRef argslist As ArrayList) As SBSValue
+        Controller.Drawer.Draw()
+        Return Nothing
+    End Function
+
+    Public Function XVDChangeItemRes(ByRef argslist As ArrayList) As SBSValue
+        Dim itemName As String = CStr(argslist(0).Value)
+        Dim resType As String = CStr(argslist(1).Value)
+        Dim resPtr As UInt16 = Controller.ResTable.GetResPtr(CStr(argslist(2).Value))
+        Dim status As UInt16 = GetStatusByName(CStr(argslist(3).Value))
+
+        Dim item As Item.ItemTag = Controller.ItemTable.GetItemByName(itemName)
+        Dim resptrs?() As UInt16 = GetResListFromItem(item, resType)
+
+        If resptrs IsNot Nothing Then
+            resptrs(status) = resPtr
+            Controller.Drawer.Update = Draw.UpdateFlag.Reflow
+        Else
+            Throw New ApplicationException("Undefined item '" + itemName + "'.")
+        End If
+
+
+        Return Nothing
+    End Function
+
+    Public Function XVDSetDrawingUpdateFlag(ByRef argslist As ArrayList) As SBSValue
+        Dim flag As String = CStr(argslist(0).Value).ToLower()
+
+        If flag = "repaint" Then
+            Controller.Drawer.Update = Draw.UpdateFlag.Repaint
+        ElseIf flag = "reflow" Then
+            Controller.Drawer.Update = Draw.UpdateFlag.Reflow
+        End If
+
+        Return Nothing
+    End Function
+
     Public Function XVDExit(ByRef argslist As ArrayList) As SBSValue
+        DebugForm.IfClose = True
         Application.Exit()
+        Return Nothing
+    End Function
+
+    Private Function GetStatusByName(ByVal name As String) As Item.EventType
+        name = name.ToLower()
+        If name = "normal" OrElse name = "" Then
+            Return Item.EventType.Normal
+        ElseIf name = "hover" Then
+            Return Item.EventType.Hover
+        ElseIf name = "press" Then
+            Return Item.EventType.Press
+        End If
+        Return Nothing
+    End Function
+
+    Private Function GetResListFromItem(ByRef item As Item.ItemTag, ByVal resType As String) As UInt16?()
+        resType = resType.ToLower()
+        If resType = "image" Then
+            Return item.Content.Image
+        ElseIf resType = "text" Then
+            Return item.Content.Text
+        ElseIf resType = "style" Then
+            Return item.Content.Style
+        ElseIf resType = "script" Then
+            Return item.Content.Script
+        End If
         Return Nothing
     End Function
 End Class
